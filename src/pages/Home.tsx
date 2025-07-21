55cbc72712c2f231ef26c3c8646577f788f3847e
Auchitya <auchi.khot27@gmail.com>
2025-07-21 18:26:21 +0530

    deleted: card grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import Navbar from "../components/Navbar"
-import CardGrid from "./CardGrid"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <CardGrid />
     </div>
   )
 }
